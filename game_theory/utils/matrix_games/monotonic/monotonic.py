c765fc21b03a7c4457a53a6c2650857a474e0560
Alexey N. Alexandrov <Alexey.N.Alexandrov@kaspersky.com>
2024-03-12 07:34:49 +0300

    fix(rk-01): monotonic accuracy errors fixed

--- game_theory/utils/matrix_games/monotonic/monotonic.py
+++ game_theory/utils/matrix_games/monotonic/monotonic.py
@@ -41,11 +41,11 @@ class Monotonic:
         self.iteration_number = 0
         _logger.info("Итерация 0:")
         # Выбираем произвольную (x^0) чистую стратегию (выставляя 1 только в одну позицию).
-        self.strategy_index = random.randint(0, m - 1)
+        strategy_index: IndexType = random.randint(0, m - 1)
         self.strategy_x = np.array([0] * n)
-        self.strategy_x[self.strategy_index] = 1
+        self.strategy_x[strategy_index] = 1
         # Выбираем вектор (c^0), соответствующий выбранной стратегии.
-        self.scores_c: np.ndarray = matrix[self.strategy_index].copy()
+        self.scores_c: np.ndarray = matrix[strategy_index].copy()
         # Текущая цена игры.
         self.price_v = np.min(self.scores_c)
         # Вектор-индикатор, который показывает принадлежность к множеству.
@@ -58,41 +58,51 @@ class Monotonic:
             optimal_strategy_x_, optimal_scores_c_, alpha_values = self.perform_iteration(matrix)
 
             alpha, _ = alpha_values
-            self.strategy_x = (1 - alpha) * self.strategy_x + alpha * optimal_strategy_x_
-            self.scores_c = (1 - alpha) * self.scores_c + alpha * optimal_scores_c_
-            self.price_v = np.min(self.scores_c)
-            self.indicator_mask_j = self.scores_c == self.price_v
-            self.__log_calculated_parameters()
+            if not np.isclose(alpha, 0):
+                self.strategy_x = (1 - alpha) * self.strategy_x + alpha * optimal_strategy_x_
+                self.scores_c = (1 - alpha) * self.scores_c + alpha * optimal_scores_c_
+                self.price_v = np.min(self.scores_c)
+                self.indicator_mask_j = np.isclose(self.scores_c, self.price_v, atol=0.0001)
+                self.__log_calculated_parameters()
+
+        _logger.info(f"Получили α_{self.iteration_number} = {alpha}, поэтому останавливаем алгоритм.")
 
         return self.price_v, self.strategy_x.copy()
 
-    def perform_iteration(self, matrix: np.ndarray[ValueType]) -> tuple[ndarray, ndarray, ndarray]:
+    def perform_iteration(self, matrix: np.ndarray[ValueType], accuracy=3) -> tuple[ndarray, ndarray, ndarray]:
         self.iteration_number += 1
         i = self.iteration_number
         _logger.info(f"Итерация {self.iteration_number}:")
         # Выбираем только столбцы, удовлетворяющие нашему индикатору.
         sub_game_matrix_a = GameMatrix(matrix[:, self.indicator_mask_j].copy())
-        _logger.info(f"Рассмотрим подыгру Г^{i}: " f"\n{np.around(sub_game_matrix_a.matrix, 3)}")
+        _logger.info(f"Рассмотрим подыгру Г^{i}: " f"\n{np.round(sub_game_matrix_a.matrix, accuracy)}")
         # Решаем подыгру и находим оптимальную стратегию x_.
         _, optimal_strategy_x_ = sub_game_matrix_a.solve()
         optimal_scores_c_: np.ndarray = self.__reduce_sum(matrix, np.array(optimal_strategy_x_))
         _logger.info(
             f"Оптимальная стратегия игрока: "
-            f"\n\t‾x_{i} = {optimal_strategy_x_}"
-            f"\n\t‾c_{i} = {tuple(optimal_scores_c_)}"
+            f"\n\t‾x_{i} = {np.round(optimal_strategy_x_, accuracy)}"
+            f"\n\t‾c_{i} = {np.round(optimal_scores_c_, accuracy)}"
         )
         # Находим оптимальную стратегию игрока в подыгре из двух строк.
         sub_game_gamma = GameMatrix(np.stack((self.scores_c, optimal_scores_c_)))
         _logger.info(
-            f"Находим оптимальную стратегию игрока в подыгре из двух строк: " f"\n{np.around(sub_game_gamma.matrix, 3)}"
+            f"Находим оптимальную стратегию игрока в подыгре из двух строк: "
+            f"\n{np.round(sub_game_gamma.matrix, accuracy)}"
         )
         sub_game_gamma = sub_game_gamma.reduce_dimension(method="nbr_drop")
-        _logger.info(f"Матрица после уменьшения размерности: " f"\n{np.around(sub_game_gamma.matrix, 3)}")
-        _, alpha_values = sub_game_gamma.solve()
-        alpha_values = (alpha_values[1], alpha_values[0])
         _logger.info(
-            f"В результате получена оптимальная стратегия (α_{i}, 1 - α_{i}) = " f"{np.around(alpha_values, 3)}"
+            f"Матрица после уменьшения размерности: "
+            f"\n{np.round(sub_game_gamma.reduce_dimension().matrix, accuracy)}"
         )
+        _, alpha_values = sub_game_gamma.solve()
+        alpha_values = (alpha_values[1] if len(alpha_values) == 2 else 0, alpha_values[0])
+        _logger.info(
+            f"В результате получена оптимальная стратегия: "
+            f"(α_{i}, 1 - α_{i}) = "
+            f"{np.round(alpha_values, accuracy)}"
+        )
+
         return np.array(optimal_strategy_x_), optimal_scores_c_, np.array(alpha_values)
 
     @staticmethod
@@ -100,14 +110,14 @@ class Monotonic:
         return np.sum((rhs * lhs.T).T, 0)
 
     def __log_calculated_parameters(self, accuracy=3):
-        j_indexes = list(*np.where(self.indicator_mask_j))
+        j_indexes = [i + 1 for i in list(*np.where(self.indicator_mask_j))]
         _logger.info(
             "\n".join(
                 [
-                    f"x^{self.iteration_number} = {np.around(self.strategy_x, accuracy)}",
-                    f"c^{self.iteration_number} = {np.around(self.scores_c, accuracy)}",
-                    f"v^{self.iteration_number} = {round(self.price_v, accuracy)}",
-                    f"J^{self.iteration_number} = {j_indexes}",
+                    f"\tx^{self.iteration_number} = {np.round(self.strategy_x, accuracy)}",
+                    f"\tc^{self.iteration_number} = {np.round(self.scores_c, accuracy)}",
+                    f"\tv^{self.iteration_number} = {round(self.price_v, accuracy)}",
+                    f"\tJ^{self.iteration_number} = {j_indexes}",
                 ]
             )
         )
